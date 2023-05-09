000b28a587e2020f0d4d74339dc1302c8d022739
Leonardo2M <leonardomm.contato@gmail.com>
2023-05-09 19:39:45 -0300

    feat(Cliente): transformando a classe em uma entidade JPA

--- gerenciador/src/main/java/br/com/gugas/gerenciador/domain/model/cliente/CPF.java
+++ gerenciador/src/main/java/br/com/gugas/gerenciador/domain/model/cliente/CPF.java
@@ -1,10 +1,14 @@
 package br.com.gugas.gerenciador.domain.model.cliente;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Embeddable
 public class CPF {
     private String numero;
 
--- gerenciador/src/main/java/br/com/gugas/gerenciador/domain/model/cliente/Cliente.java
+++ gerenciador/src/main/java/br/com/gugas/gerenciador/domain/model/cliente/Cliente.java
@@ -1,6 +1,7 @@
 package br.com.gugas.gerenciador.domain.model.cliente;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "clientes")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String nome;
+    @Embedded
     private CPF cpf;
+    @Embedded
     private Endereco endereco;
 
 }
